b42900610a832736927f12a12b825a79db971e06
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-16 15:24:55 +0300

    sunucu çalıştırıldı.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // server için gerekli olanları burada ayarlayın
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("AYSEN");
+});
+
+module.exports = server;
 
 // posts router'ını buraya require edin ve bağlayın
