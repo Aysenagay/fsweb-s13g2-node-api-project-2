1b785e76f2c0086bed78f5def722a735cebf9069
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-16 16:27:16 +0300

    GET YAPILDI

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,37 @@
 // posts için gerekli routerları buraya yazın
 const router = require("express").Router();
+const Posts = require("./posts-model");
 
 //GET
+
+router.get("/", (req, res) => {
+  Posts.find()
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Gönderiler alınamadı" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Gönderi bilgisi alınamadı.",
+      });
+    });
+});
+
+//POST
 
 module.exports = router;
