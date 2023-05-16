faef92466b27d3d0da95d1cbeecff78b5c585151
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-16 15:50:56 +0300

    taslak

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 // server için gerekli olanları burada ayarlayın
 const express = require("express");
 const server = express();
+const postsRouter = require("./posts/posts-router");
 
 server.use(express.json());
+
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
   res.send("AYSEN");
